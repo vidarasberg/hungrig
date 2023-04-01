5137ecac736ed775bb78d2fe6623206856df953f
Vidar Åsberg <vidar.asberg@protonmail.com>
2023-04-01 17:27:18 +0200

    Adds adding recipe for logged in users

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,62 @@
 import { api } from "@/utils/api";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+  useUser,
+} from "@clerk/nextjs";
 import { type NextPage } from "next";
 import Head from "next/head";
+import { useState } from "react";
+
+const CreateRecipeWizard = () => {
+  const { user } = useUser();
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const { mutate } = api.recipes.create.useMutation();
+  if (!user) return null;
+
+  return (
+    <div>
+      <label htmlFor="name">Name:</label>
+      <input
+        type="text"
+        id="name"
+        name="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <label htmlFor="link">Link:</label>
+      <input
+        type="text"
+        id="link"
+        name="link"
+        value={link}
+        onChange={(e) => setLink(e.target.value)}
+      />
+      <button onClick={() => mutate({ name: name, link: link })}>Submit</button>
+    </div>
+  );
+};
+
+const Recipes = () => {
+  const { data } = api.recipes.getAll.useQuery();
+  return (
+    <>
+      {data?.map((x) => (
+        <div key={x.id}>
+          <a href={x.link} target="_blank">
+            {x.name}
+          </a>
+        </div>
+      ))}
+    </>
+  );
+};
 
 const Home: NextPage = () => {
-  const { data } = api.recipes.getAll.useQuery();
+  const { user } = useUser();
 
   return (
     <>
@@ -26,13 +78,12 @@ const Home: NextPage = () => {
         </div>
       </header>
       <main className="flex flex-col items-center p-4">
-        {data?.map((x) => (
-          <div key={x.id}>
-            <a href={x.link} target="_blank">
-              {x.name}
-            </a>
-          </div>
-        ))}
+        {user && (
+          <>
+            <CreateRecipeWizard />
+            <Recipes />
+          </>
+        )}
       </main>
     </>
   );
--- src/server/api/routers/recipe.ts
+++ src/server/api/routers/recipe.ts
@@ -1,7 +1,30 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { z } from "zod";
 
 export const recipesRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.recipe.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const recipes = await ctx.prisma.recipe.findMany();
+    return recipes;
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        name: z.string().min(1).max(255),
+        link: z.string().min(1).max(255).url(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.currentUser;
+
+      const recipe = await ctx.prisma.recipe.create({
+        data: { authorId, ...input },
+      });
+
+      return recipe;
+    }),
 });
